d3beca2e4fc4c02c1d0c4852d0be7ffd11569595
Amir Sohi <amir.sohi@cazoo.co.uk>
2022-06-19 23:04:47 +0100

    styled cells

--- src/components/Heatmap/HeatmapView.tsx
+++ src/components/Heatmap/HeatmapView.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Transactions } from "../../features/transaction-data/transactionType";
+import './Heatmap.css'
 
-interface HeatmapViewProps {
+type HeatmapViewProps = {
     transactions: Transactions | null;
 }
+
+const Cell: React.FC = () => <div className='cell' role="cell" />
 
 export const HeatmapView: React.FC<HeatmapViewProps> = ({ transactions }) => {
     return (
         <div className="heatmap-cells-wrapper">
-            {transactions !== null && Object.keys(transactions).map(transaction => <div role="cell" key={transaction}>Cell</div>)}
+            {transactions !== null && Object.keys(transactions).map(transaction => <Cell key={transaction}/>)}
         </div>)
 };
